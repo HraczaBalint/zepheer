be253cec895898492a0b5b97e3e3ca570aca0dbf
HraczaBalint <hraczabalintmail@gmail.com>
2022-01-07 15:44:33 +0100

    CORS Fixed

--- backend/index.php
+++ backend/index.php
@@ -31,10 +31,14 @@ $dbManager->bootEloquent();
 
 $app->setBasePath('/zepheer/backend');
 
+$app->options('/{routes:.+}', function ($request, $response, $args) {
+    return $response;
+});
+
 $app->add(function ($request, $handler) {
     $response = $handler->handle($request);
     return $response
-        ->withHeader('Access-Control-Allow-Origin', 'http://localhost:3000')
+        ->withHeader('Access-Control-Allow-Origin', 'http://localhost:3000') // vagy '*'
         ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
         ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
 });
