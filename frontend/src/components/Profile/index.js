e646771766855467304a0176ea96b3c5dce8fcf3
HraczaBalint <hraczabalintmail@gmail.com>
2022-02-03 15:11:14 +0100

    User profile

--- frontend/src/components/Profile/index.js
+++ frontend/src/components/Profile/index.js
@@ -1,22 +1,13 @@
 import React from 'react';
+import { ApiContext } from "../../api/api";
 
 export default class Profile extends React.Component{
+    static contextType = ApiContext;
 
     constructor(props) {
         super(props);
-
         this.state = {
-            userProfile: {
-                user_id: 1,
-                user_name: "Jani",
-                user_password: "jelszo",
-                user_email: "jani@gmail.com",
-                user_gender: 1,
-                user_gender_preference: 0,
-                user_age: 20,
-                user_age_preference: "19.756",
-                user_description: "Hello there",
-            }
+
         }
     }
 
