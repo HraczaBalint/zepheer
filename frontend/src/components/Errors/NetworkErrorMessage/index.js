f1f1a39f1e6acb90ce3918f53725bea083f96106
HraczaBalint <hraczabalintmail@gmail.com>
2022-02-04 15:13:35 +0100

    Network error message fix

--- frontend/src/components/Errors/NetworkErrorMessage/index.js
+++ frontend/src/components/Errors/NetworkErrorMessage/index.js
@@ -6,25 +6,11 @@ import { Alert } from "react-bootstrap";
 export default class NetworkErrorMessage extends React.Component{
     static contextType = ApiContext;
 
-    constructor (props) {
-        super(props);
-        this.state = {
-            networkError: true,
-            handleShowError: this.handleShowError,
-        };
-    }
-
-    handleShowError = () => {
-        this.setState({
-            networkError: !this.state.networkError,
-        })
-    }
-
     render() {
         if (this.context.networkError) {
             return(
                 <>
-                    <Alert show={this.state.networkError} variant="danger" onClose={this.handleShowError} dismissible>
+                    <Alert show={this.context.networkError} variant="danger">
                     <Alert.Heading>Oh snap! You got an error!</Alert.Heading>
                     <p>Pleasse check your internet connection and try again.</p>
                     </Alert>
